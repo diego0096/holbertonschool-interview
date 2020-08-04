2b146aec73143554483d2768c835f142dba3149b
diego0096 <dfqz93@hotmail.com>
2020-08-04 21:38:48 +0000

    binary_trees.h

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -18,26 +18,12 @@ typedef struct binary_tree_s
     struct binary_tree_s *right;
 } binary_tree_t;
 
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/**
- * struct node_depth - a heap_t node and how deep in a tree it is. Used for a
- * pair value return in _swap in 1-heap_insert.c. Style checker requires
- * it in header even though this isn't intended for anywhere else.
- *
- * @node: the node
- * @depth: how deep it is
- */
-typedef struct node_depth
-{
-    heap_t *node;
-    size_t depth;
-} node_depth;
-
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
 heap_t *heap_insert(heap_t **root, int value);
 
-void binary_tree_print(const binary_tree_t *tree);
-
-#endif
+#endif /*_BINARY_TREES_H_*/
