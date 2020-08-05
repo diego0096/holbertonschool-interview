fe7d3670e8719724dcd9718181a8c1ac0701cf22
diego0096 <dfqz93@hotmail.com>
2020-08-05 03:25:34 +0000

    swap the node

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -27,3 +27,42 @@ node_depth _find_insert_parent(heap_t *node, size_t depth)
 	else
 		return (ret);
 }
+
+/**
+ * _swap - swap a node with its parent
+ *
+ * @node: node to swap with its parent
+ */
+void _swap(heap_t *node)
+{
+	heap_t *swap, *parent;
+
+	parent = node->parent;
+	if (node->left != NULL)
+		node->left->parent = parent;
+	if (node->right != NULL)
+		node->right->parent = parent;
+	if (parent->left != NULL)
+		parent->left->parent = node;
+	if (parent->right != NULL)
+		parent->right->parent = node;
+	swap = node->left;
+	node->left = parent->left;
+	parent->left = swap;
+	swap = node->right;
+	node->right = parent->right;
+	parent->right = swap;
+	if (node == node->left)
+		node->left = parent;
+	if (node == node->right)
+		node->right = parent;
+	if (parent->parent != NULL)
+	{
+		if (parent->parent->left == parent)
+			parent->parent->left = node;
+		if (parent->parent->right == parent)
+			parent->parent->right = node;
+	}
+	node->parent = parent->parent;
+	parent->parent = node;
+}
