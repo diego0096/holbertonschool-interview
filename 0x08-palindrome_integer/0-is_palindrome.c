1a53a4a877c3f52efe5e90ceefe9f9a0e1af198d
diego0096 <dfqz93@hotmail.com>
2020-09-17 03:12:09 +0000

    palindrome

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - function that checks if a given integer is a palindrome
+ * @n: unsigned long integer
+ * Return: 1 if integer is a palindrome, 0 otherwise
+ * 
+ */
+
+int is_palindrome(unsigned long n)
+{
+	int len = 0, j = 0;
+	char str[21];
+	len = sprintf(str, "%lu", n);
+	for (j = 0; j < len; j++)
+		if (str[j] != str[len - 1 - j])
+			return (0);
+	return (1);
+}
