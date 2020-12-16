c44efc441190b19b88394ef9d9a6f447084dd08a
diego0096 <dfqz93@hotmail.com>
2020-12-16 14:51:08 +0000

    coun

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -7,6 +7,7 @@ import sys
 def count_words(subreddit, word_list, kw_cont={}, next_pg=None, reap_kw={}):
     """all hot posts by keyword"""
     headers = {"User-Agent": "julgachancipa"}
+
     if next_pg:
         subRhot = requests.get('https://reddit.com/r/' + subreddit +
                                '/hot.json?after=' + next_pg,
@@ -14,16 +15,20 @@ def count_words(subreddit, word_list, kw_cont={}, next_pg=None, reap_kw={}):
     else:
         subRhot = requests.get('https://reddit.com/r/' + subreddit +
                                '/hot.json', headers=headers)
+
     if subRhot.status_code == 404:
         return
+
     if kw_cont == {}:
         for word in word_list:
             kw_cont[word] = 0
             reap_kw[word] = word_list.count(word)
+
     subRhot_dict = subRhot.json()
     subRhot_data = subRhot_dict['data']
     next_pg = subRhot_data['after']
     subRhot_posts = subRhot_data['children']
+
     for post in subRhot_posts:
         post_data = post['data']
         post_title = post_data['title']
@@ -32,12 +37,15 @@ def count_words(subreddit, word_list, kw_cont={}, next_pg=None, reap_kw={}):
             for key in kw_cont:
                 if w.lower() == key.lower():
                     kw_cont[key] += 1
+
     if next_pg:
         count_words(subreddit, word_list, kw_cont, next_pg, reap_kw)
+
     else:
         for key, val in reap_kw.items():
             if val > 1:
                 kw_cont[key] *= val
+
         sorted_abc = sorted(kw_cont.items(), key=lambda x: x[0])
         sorted_res = sorted(sorted_abc, key=lambda x: (-x[1], x[0]))
         for res in sorted_res:
