d9799dd341c8701381222d7fe43ba2cd7a121a9e
diego0096 <dfqz93@hotmail.com>
2021-01-12 16:01:06 +0000

    mul

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,151 +1,149 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - multiplies two positive numbers
- * @argc: argument counter
- * @argv: argument vector
- * Return: 0
+ * main - program that multiplies two positive numbers
+ * @argc: argument count, must be 3
+ * @argv: arguments, argv[1] and argv[2]
+ * Return: product of argv[1] by argv[2]
  */
+
 int main(int argc, char *argv[])
 {
-	char *error = "Error\n";
-	int i;
+	char *num1, *num2;
+	int i, j, k, len1, len2, len, d1, d2, d1d2, carry, *mul;
 
-	if (argc != 3 || !is_a_number(argv[1]) || !is_a_number(argv[2]))
-	{
-		for (i = 0; error[i] != '\0'; i++)
-		{
-			_putchar(error[i]);
-		}
+	if (argc != 3 || !(_isnumber(argv[1])) || !(_isnumber(argv[2])))
+		_error(), exit(98);
+	num1 = argv[1], num2 = argv[2];
+	len1 = _strlen(num1), len2 = _strlen(num2), len = len1 + len2;
+	mul = _calloc(len, sizeof(int));
+	if (mul == NULL)
 		exit(98);
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		d1 = num1[i] - '0';
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			d2 = num2[j] - '0';
+			d1d2 = d1 * d2;
+			mul[i + j + 1] += d1d2 % 10;
+			carry = d1d2 / 10;
+			if (mul[i + j + 1] > 9)
+			{
+				mul[i + j] += mul[i + j + 1] / 10;
+				mul[i + j + 1] = mul[i + j + 1] % 10;
+			}
+			mul[i + j] += carry;
+		}
 	}
-	multiply(argv[1], argv[2]);
+	for (k = 0; mul[k] == 0 && k < len; k++)
+		;
+	if (k == len)
+		_putchar(mul[len - 1] + '0');
+	else
+	{
+		for (i = k; i < len; i++)
+			_putchar(mul[i] + '0');
+	}
+	_putchar('\n');
+	free(mul);
 	return (0);
 }
 
-
 /**
- * multiply - Fills an array with the multiplication num_1 times i
- * Arguments
- * @num_1:	string representing the 1st number in 10 base
- * @num_2:  string representing the 2nd number in 10 base
- * Returns - The multiplication
+ * _isnumber - checks for digit-only (0 through 9) numbers
+ * @str: parameter hard-coded in main
+ * Return: 1 or 0
  */
 
-void multiply(char *num_1, char *num_2)
-{
-	char mul[10][MAX1], answer[MAX1][MAX2];
-	int tag, i, j, len_1, len_2, res, value, pos_1, pos_2, row, z;
-
-	for (len_1 = str_len(num_1), len_2 = str_len(num_2), i = 0; i < 10; i++)
-		for (j = 0; j < MAX1; j++)
-			mul[i][j] = '.';
-	for (i = 0; i < MAX1; i++)
-		for (j = 0; j < MAX2; j++)
-			answer[i][j] = '.';
-	for (tag = 0, i = 0; i < 10; i++)
-	{
-		for (j = 0; j <= len_1; j++)
-		{
-			pos_1 = len_1 - j - 1;
-			if (pos_1 >= 0)
-			{	res = ((num_1[pos_1] - 48) * i) + tag;
-				value = res % 10;
-				mul[i][j] = (value + '0');
-				tag = res / 10;
-			}
-			else
-			{	mul[i][j] = (tag != 0) ? tag + 48 : '.';
-				tag = 0;
-			}
-		}
-	}
-	for (j = 0; j < len_2; j++)
-	{	pos_2 = len_2 - j - 1;
-		row = num_2[pos_2] - 48;
-		for (z = 0; z < len_1 + 1; z++)
-			answer[j][z + j] = mul[row][z];
-	}
-	sum_and_print(answer, len_1, len_2);
-}
-
-/**
- * is_a_number - Indicates if a string represents a number in 10 base
- * Arguments
- * @a_str: string representing a number in 10 base
- * Return: 1 if true, 0 if not.
- */
-int is_a_number(char *a_str)
-{
-	while (*a_str != '\0')
-	{
-		if (*a_str < '0' || *a_str > '9')
-			return (_FALSE_);
-		a_str++;
-	}
-	return (_TRUE_);
-}
-
-/**
- * sum_and_print - Generates the sumation and print the results
- * Arguments
- * @a:	matrix with multiplitation tables
- * @len_1: len of string 1
- * @len_2: len os string 2
- * Return: noting.
- */
-void sum_and_print(char a[MAX1][MAX2], int len_1, int len_2)
-{
-	int tag, i, j, sum, value;
-	char total[MAX2], ans[MAX2 - 1];
-	char *ar;
-
-	tag = 0;
-	for (j = 0; j <= (len_1 + len_2); j++)
-	{
-		sum = 0;
-		for (i = 0; i < len_2; i++)
-			sum += (a[i][j] >= '0' && a[i][j] <= '9') ? a[i][j] - 48 : 0;
-		value = (sum + tag) % 10;
-		total[j] = (value + '0');
-		tag = (sum + tag) / 10;
-	}
-
-	for (i = 0; i < len_1 + len_2; i++)
-	{
-		ans[i] = total[len_1 + len_2 - 1 - i];
-	}
-	ans[i] = '\0';
-
-	value = str_len(ans);
-	for (i = 0; i < value; i++)
-	{
-		if (ans[i] == '0')
-			ar = &ans[i + 1];
-		else
-		{
-			ar = &ans[i];
-			i = value;
-		}
-	}
-	for (i = 0; ar[i] != '\0'; i++)
-	{
-		_putchar(ar[i]);
-	}
-	putchar('\n');
-}
-
-/**
- * str_len - Calculates the lenght of a string
- * Arguments
- * @a:	matrix with multiplitation tables
- * Return: The lengnth of a.
- */
-int str_len(char *a)
+int _isnumber(char *str)
 {
 	int i;
 
-	for (i = 0; a[i] != '\0'; i++)
-	{}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * _error - print error
+ * Return: void
+ */
+
+void _error(void)
+{
+	int i;
+	char error[] = "Error";
+
+	for (i = 0; i < 5; i++)
+		_putchar(error[i]);
+	_putchar('\n');
+}
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: parameter defined in main
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
 	return (i);
+}
+
+/**
+ * _calloc - function that allocates memory for an array, using malloc
+ * @nmemb: size of the memory space to allocate in bytes
+ * @size: size of type
+ * Return: void pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	_memset(ptr, 0, size * nmemb);
+	return (ptr);
+}
+
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: pointer to memory area
+ * @b: constant byte
+ * @n: number of bytes to be filled
+ * Return: memory address of function (memory area)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	char *tmp = s;
+
+	for (i = 0; i < n; i++)
+	{
+		*s = b;
+		s++;
+	}
+	s = tmp;
+	return (s);
 }
